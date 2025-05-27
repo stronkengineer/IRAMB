1cbcbcc8f110e63668b417055346dc6191c390e9
adam <adamelnaba@gmail.com>
2025-05-27 04:57:39 +0300

    im about to kms

--- test.py
+++ test.py
@@ -1,4 +1,2 @@
-from binance.client import Client
-
-client = Client("NPXlmdB5YkQmjsmv2ZrtrCirHVQSBIhQMOkBfA4NkfBQVkzA8XK2Hp9N53E5WPoo", "Iwlz5wVmszSThrvzGXdiIWcdQ6AvF7hA7cpoY7LxGLKMIHKObKqTGa8wmkbmXbUU")
-print(client.get_account_status())  
+import ccxt
+print(ccxt.exchanges)
